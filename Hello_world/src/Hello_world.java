983db44afc0159353ca7beaed222003611d81b77
myshkin91 <18solovieva45@millshighsch.org>
2017-08-21 08:17:40 -0700

    Hello_World

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,18 @@
+
+public class Hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 * Write a description of class Hello_world here.
+		 * 
+		 * @author (Alex Soloviev) 
+		 * @version (8/21/2017)
+		 */
+		
+		        System.out.println("Hello World! From <Alex Soloviev>"); //Display the string
+		    }
+	
+
+	}
+
