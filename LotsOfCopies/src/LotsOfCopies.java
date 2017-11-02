3c844dab6b9960b37b8eea562e30f0795c217efa
Sasha <Sasha@192.168.1.133>
2017-11-01 23:45:44 -0700

    Array Mini Lab

---
+++ LotsOfCopies/src/LotsOfCopies.java
@@ -0,0 +1,41 @@
+//Alex Soloviev
+//November 1,2017
+
+import java.util.*;
+public class LotsOfCopies {
+	public static void main(String[] args) {
+	int num = 7;
+	String strMain = "APCS";
+	int[] arrMain = {1, 2, 3, 4, 5};
+	changeMe(num, strMain, arrMain);
+	System.out.println(num); 
+	System.out.println(strMain);
+	System.out.println(Arrays.toString(arrMain)); 
+	aTob();
+	
+}
+
+public static void changeMe(int x, String str, int[] arr) {
+	x = 8;
+	str = "SCPA";
+	for (int i = 0; i < arr.length; i++) {
+		arr[i] += 1;
+	}
+}
+public static void  aTob() {
+	int numA = 1;
+	int numB = numA;
+	numA += 1;
+	System.out.println(numB); 
+	String strA = "Hello";
+	String strB = strA;
+	strA = "World";
+	System.out.println(strB); 
+	int[] arrA = {2, 4, 6, 8};
+	int[] arrB = arrA;
+	for (int i = 0; i < arrA.length; i++) {
+		arrA[i] += 1;
+	} 
+	System.out.println(Arrays.toString(arrB));
+}
+}
