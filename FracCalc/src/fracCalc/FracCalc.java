58dcaceb7ecaac8cd1d4d80f674a71623bd379fb
Sasha <Sasha@192.168.1.133>
2017-11-20 20:19:34 -0800

    finished checkpoint3

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -1,4 +1,4 @@
-/*package fracCalc;
+package fracCalc;
 import java.util.*;
 public class FracCalc {
 
@@ -24,18 +24,25 @@ public class FracCalc {
     //        
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    { 
+    public static String produceAnswer(String input) {
+    
         // TODO: Implement this function to produce the solution to the input
         String [] inputArray = input.split(" ");
         int [] operand1 = splitOperand(inputArray[0]);
         int [] operand2 = splitOperand(inputArray[2]);
+        String result = "";
         String operator = inputArray[1];
-        //String result;
+        
+        if(operator.equals("+") || operator.equals("-")){
+        		result = addSubtract(operand1, operand2, operator);
+        		}else if(operator.equals("*") || operator.equals("/")){
+        		result = multiplyDivide(operand1, operand2, operator);
+        		}return result;
+    }		
         public static int [] splitOperand(String operand) {
         	int numerator = 0;
             int denominator = 1;
-           	 	int wholeNumber = 0;
+           		int wholeNumber = 0;
                 if(operand.contains("/") && operand.contains("_")) {
                 	numerator = (Integer.parseInt(operand.split("_")[1].split("/")[0]));
                 	denominator = (Integer.parseInt(operand.split("/")[1]));
@@ -46,22 +53,47 @@ public class FracCalc {
                 }else{
                 	wholeNumber = Integer.parseInt(operand);
                }
-           /*     String numerator2 = "0";
-                String denominator2 = "1";
-           	 	String wholeNumber2 = "0";*/
-              /*  if(inputArray[2].contains("/") && inputArray[2].contains("_")) {
-                	numerator2 = (inputArray[2].split("_")[1].split("/")[0]);
-                	denominator2 = (inputArray[2].split("/")[1]);
-                	wholeNumber2 = (inputArray[2].split("_")[0]);
-                }else if(inputArray[2].contains("/") && !(inputArray[2].contains("_"))){
-                	numerator2 = (inputArray[2].split("/")[0]);
-                	denominator2 = (inputArray[2].split("/")[1]);
-                }else{
-                	wholeNumber2 = inputArray[2];
-                }
-        return "whole:" + wholeNumber2 + " numerator:" + numerator2 + " denominator:" + denominator2;
-    }*/
+               
+               if(wholeNumber < 0){
+               numerator *= -1;
+               }
+                int[] operandParts = {wholeNumber, numerator, denominator};
+          return operandParts;
+      }
+        public static String addSubtract (int []operand1, int [] operand2, String operator) {
+        	int newDenominator = operand1[2] * operand2[2];
+        	int numerator1 = operand1[1] * operand2[2];
+        	int numerator2 = operand2[1] * operand1[2];
+        	int newNumerator = numerator1 + numerator2;
+        	int newWholeNumber = operand1[0] + operand2[0];
+        	
+        	if(operator.equals("-")) {
+        		newNumerator = numerator1 - numerator2;
+        		newWholeNumber = operand1[0] - operand2[0];
+        	}
+        	if (newWholeNumber != 0 && newNumerator<0) {
+        		newNumerator *= -1;
+        	}
+        	return newWholeNumber + "_" + newNumerator + "/" + newDenominator;
+        	}
+        
+        public static String multiplyDivide (int [] operand1, int [] operand2, String operator) {
+        int numerator1 = operand1[0] * operand1[2] + operand1[1];
+        int numerator2 = operand2[0] * operand2[2] + operand2[1];
+        		if(operator.equals("/")) {
+        			int temp = numerator2;
+        			numerator2 = operand2[2];
+        			operand2[2] = temp;
+        		}
+        		int newNumerator = numerator1 * numerator2;
+        		int newDenominator = operand1[2] * operand2[2];
+        		return newNumerator + "/" + newDenominator;
+        		}
+        
+        }
+        
+           
 
-    // TODO: Fill in the space below with any helper methods that you think you will need
+     //TODO: Fill in the space below with any helper methods that you think you will need
     
-//}
+
