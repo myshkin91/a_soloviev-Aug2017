47921f050e98f152057a9cf2e98117d142aba9af
myshkin91 <18solovieva45@millshighsch.org>
2017-11-28 09:24:21 -0800

    Code Magnets

---
+++ Code Magnets/src/DrumKit.java
@@ -0,0 +1,13 @@
+
+ class DrumKit {
+	
+		boolean topHat = true;
+		boolean snare =true;
+		void playSnare() {
+			System.out.println("bang bang ba bang");
+		}
+		void playTopHat() {
+			System.out.println("ding ding da ding");
+		}
+	}
+
