247ce1541d025fb8be71c47afd462607ac642670
myshkin91 <18solovieva45@millshighsch.org>
2017-12-12 09:22:39 -0800

    Finished new checkpoint2

--- FracCalcNew/src/fracCalc/FracCalc.java
+++ FracCalcNew/src/fracCalc/FracCalc.java
@@ -39,119 +39,13 @@ public class FracCalc {
 		if (parsedInput.length % 2 == 0 || parsedInput.length == 1) {
 			return "ERROR: Invalid equation. Please try again.";
 		}
-		String result = "";
+		String resultString = "";
 		for (int i = 2; i < parsedInput.length; i += 2) {
-			int[] operand1 = splitOperand(parsedInput[0]);
-			int[] operand2 = splitOperand(parsedInput[i]);
-			String operator = parsedInput[i - 1];
-			int[] unsimplifiedArray = new int[2];
-			if (operand1[1] == 0) {
-				return "ERROR: Cannot divide by 0.";
-			}
-			if (operator.equals("+") || operator.equals("-")) {
-				unsimplifiedArray = addSubtract(operand1, operand2, operator);
-			} else if (operator.equals("*")) {
-				unsimplifiedArray = multiplyDivide(operand1, operand2, operator);
-			} else if (operator.equals("/")) {
-				if (operand2[1] == 0 || operand2[0] == 0) {
-					return "ERROR: Cannot divide by zero.";
-				}
-				unsimplifiedArray = multiplyDivide(operand1, operand2, operator);
-			} else {
-				return "ERROR: Invalid operator.";
-			}
-			result = simplify(unsimplifiedArray);
-			parsedInput[0] = result;
+			Fraction operand1 = new Fraction(parsedInput[0]);
+			Fraction operand2 = new Fraction(parsedInput[i]);
+			String operator = parsedInput[i-1];
+			resultString = operand2.toString();
 		}
-		return result;
-	}
-
-	// This method accepts an operand and returns the whole number, numerator, and
-	// denominator.
-	public static int[] splitOperand(String operand) {
-		int numerator = 0;
-		int denominator = 1;
-		int wholeNum = 0;
-		if (operand.contains("/") && operand.contains("_")) {
-			numerator = (Integer.parseInt(operand.split("_")[1].split("/")[0]));
-			denominator = (Integer.parseInt(operand.split("/")[1]));
-			wholeNum = (Integer.parseInt(operand.split("_")[0]));
-		} else if (operand.contains("/") && !(operand.contains("_"))) {
-			numerator = (Integer.parseInt(operand.split("/")[0]));
-			denominator = (Integer.parseInt(operand.split("/")[1]));
-		} else {
-			wholeNum = Integer.parseInt(operand);
-		}
-		if (wholeNum < 0) {
-			numerator *= -1;
-		}
-		numerator = wholeNum * denominator + numerator;
-		int[] operandParts = { numerator, denominator };
-		return operandParts;
-	}
-
-	// This method returns the sum or difference of the operands.
-	public static int[] addSubtract(int[] operand1, int[] operand2, String operator) {
-		int newDenominator = operand1[1] * operand2[1];
-		int numerator1 = operand1[0] * operand2[1];
-		int numerator2 = operand2[0] * operand1[1];
-		int newNumerator = numerator1 + numerator2;
-		if (operator.equals("-")) {
-			newNumerator = numerator1 - numerator2;
-		}
-		int[] result = { newNumerator, newDenominator };
-		return result;
-	}
-
-	// This method returns the product or quotient of the operands.
-	public static int[] multiplyDivide(int[] operand1, int[] operand2, String operator) {
-		if (operator.equals("/")) {
-			if (operand2[0] < 0) {
-				operand2[1] *= -1;
-				operand2[0] *= -1;
-			}
-			int temp = operand2[0];
-			operand2[0] = operand2[1];
-			operand2[1] = temp;
-		}
-		int newNumerator = operand1[0] * operand2[0];
-		int newDenominator = operand1[1] * operand2[1];
-		int[] result = { newNumerator, newDenominator };
-		return result;
-	}
-
-	// This method simplifies the result.
-	public static String simplify(int[] operand) {
-		int wholeNum = operand[0] / operand[1];
-		int numerator = operand[0] % operand[1];
-		int denominator = operand[1];
-		if (wholeNum != 0) {
-			numerator = Math.abs(numerator);
-		}
-		int gcf = GCF(numerator, denominator);
-		numerator /= gcf;
-		denominator /= gcf;
-		if (wholeNum == 0) {
-			if (numerator == 0) {
-				return "0";
-			}
-			return numerator + "/" + denominator;
-		}
-		if (numerator == 0) {
-			return "" + wholeNum;
-		}
-		return wholeNum + "_" + numerator + "/" + denominator;
-	}
-
-	// This method accepts two integers and returns the greatest common factor.
-	public static int GCF(int num1, int num2) {
-		num1 = Math.abs(num1);
-		num2 = Math.abs(num2);
-		for (int i = num1; i > 1; i--) {
-			if (num1 % i == 0 && num2 % i == 0) {
-				return i;
-			}
-		}
-		return 1;
+		return resultString;
 	}
 }
--- FracCalcNew/src/fracCalc/Fraction.java
+++ FracCalcNew/src/fracCalc/Fraction.java
@@ -2,16 +2,14 @@ package fracCalc;
 
 public class Fraction {
 
-	private int numerator;
-	private int denominator;
-	private int whole;
+	private int numerator = 0;
+	private int denominator = 1;
+	private int whole = 0;
+	
 	public Fraction(String fractionStr) {
 		parsingMethod(fractionStr);
 	}
 	private void parsingMethod(String operand) {
-		int numerator = 0;
-		int denominator = 1;
-		int whole = 0;
 		if (operand.contains("/") && operand.contains("_")) {
 			numerator = (Integer.parseInt(operand.split("_")[1].split("/")[0]));
 			denominator = (Integer.parseInt(operand.split("/")[1]));
@@ -22,10 +20,8 @@ public class Fraction {
 		} else {
 			whole = Integer.parseInt(operand);
 		}
-		if (whole < 0) {
-			numerator *= -1;
-		}
-		numerator = whole * denominator + numerator;
-		int[] operandParts = { numerator, denominator };
+	}
+	public String toString() {
+		return "whole:" + whole + " numerator:" + numerator + " denominator:" + denominator;
 	}
 }
