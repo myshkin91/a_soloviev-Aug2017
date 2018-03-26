024cf2a8d55b5b7632fbe6ce104f253cedee1abb
Sasha <Sasha@alexanders-mbp>
2018-03-25 23:32:51 -0700

    checkpoint 2 processCommand

--- TextExcel/src/textExcel/Spreadsheet.java
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -1,39 +1,30 @@
 package textExcel;
 
 public class Spreadsheet implements Grid {
-	Cell[][] thisSheet;
+	private Cell[][] thisSheet;
 	
 	public Spreadsheet(){
 		thisSheet = new Cell[getRows()][getCols()]; 
-		reset();
+		clear();
 		}
+	
 	public String processCommand(String command){
 		
-		String[] parsed = command.split(" ",3);
-		
-		if(parsed.length == 1) {
-			if (Character.isAlphabetic(Character.toLowerCase(command.charAt(0))) && Character.isDigit(command.charAt(1))) {
-				Location thisCell = new SpreadsheetLocation(command);
-				return getCell(thisCell).fullCellText();
-			} else if (parsed.length == 2) {
-				if(command.toLowerCase().startsWith("clear")) {
-					Location thisCell = new SpreadsheetLocation(parsed[1]);
-					thisSheet[thisCell.getRow()][thisCell.getCol()] = new EmptyCell();
-					return getGridText();
-				}
-			} else {
-				if(Character.isAlphabetic(Character.toLowerCase(command.charAt(0))) && Character.isDigit(command.charAt(1))) {
-					if(parsed[1].equals("=")) {
-						if(parsed[2].startsWith("" + '"')) {
-							Location thisCell = new SpreadsheetLocation(parsed[0].toLowerCase());
-							String input = parsed[2].substring(1, (parsed[2].length() - 1));
-							thisSheet[thisCell.getRow()][thisCell.getCol()] = new TextCell(input);
-							return getGridText();
-						}
-					}
-				}
-			}
-		}return "";
+		String lCommand = command.toLowerCase();
+		if(lCommand.equals("clear")) {
+			clear();
+		}
+		else (lCommand.startsWith("clear")){
+			SpreadsheetLocation loc = new SpreadsheetLocation(command.split(" ")[1]);
+			thisSheet[loc.getRow()][loc.getCol()] = new EmptyCell();
+		}else if (command.contains("=")) {
+			String [] parts = command.split(" ", 3);
+			SpreadsheetLocation loc = new SpreadsheetLocation(parts[0]);
+			thisSheet[loc.getRow()][loc.getCol()] = new TextCell(parts[2].substring(1, parts[2].length() - 1));
+		}else {
+			SpreadsheetLocation loc = new SpreadsheetLocation(command);
+			return thisSheet[loc.getRow()][loc.getCol()].fullCellText();
+		}return getGridText();
 	}
 
 	
@@ -73,7 +64,7 @@ public class Spreadsheet implements Grid {
 		}
 		return output;
 	}	
-	private void reset() {
+	private void clear() {
 		for (int r = 0; r< thisSheet[0].length; r++) {
 			for (int c = 0; c< thisSheet.length; c++) {
 				thisSheet[c][r] = new EmptyCell();
