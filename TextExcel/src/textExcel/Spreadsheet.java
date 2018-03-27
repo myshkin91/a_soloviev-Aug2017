6353ade05e13a0443b9fd31c73ad691515a2ff95
myshkin91 <18solovieva45@millshighsch.org>
2018-03-27 09:18:03 -0700

    finished chp2

--- TextExcel/src/textExcel/Spreadsheet.java
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -9,19 +9,17 @@ public class Spreadsheet implements Grid {
 		}
 	
 	public String processCommand(String command){
-		
+		String [] parts = command.split(" ", 3);
 		String command1 = command.toLowerCase();
 		if(command1.equals("clear")) {
 			clear();
-		}
-		else if(command1.startsWith("clear")){ 
+		}else if(command1.startsWith("clear")){ 
 			SpreadsheetLocation loc = new SpreadsheetLocation(command.split(" ")[1]);
 			thisSheet[loc.getRow()][loc.getCol()] = new EmptyCell();
 		}else if (command.contains("=")) {
-			String [] parts = command.split(" ", 3);
-			SpreadsheetLocation loc = new SpreadsheetLocation(parts[0]);
-			thisSheet[loc.getRow()][loc.getCol()] = new TextCell(parts[2].substring(1, parts[2].length() - 1));
-		}else {
+			assignValue(parts[0],parts[2]);
+			}
+		else {
 			SpreadsheetLocation loc = new SpreadsheetLocation(command);
 			return thisSheet[loc.getRow()][loc.getCol()].fullCellText();
 		}
@@ -47,29 +45,32 @@ public class Spreadsheet implements Grid {
 	public String getGridText(){
 		String output = "   ";
 		
-		for(int i = 0; i < thisSheet.length; i++) {
+		for(int i = 0; i < thisSheet[0].length; i++) {
 			output = output + "|" + ((char)(65 + i)) + "         ";
 	}
 		output += "|\n";
-		for(int r = 0; r<thisSheet[0].length; r++) {
+		for(int r = 0; r <thisSheet.length; r++) {
 			String rowNumber = "" + (r + 1);
 			while(rowNumber.length()<3) {
 				rowNumber += " ";
 			}
 			output += rowNumber;
 			for(int c = 0;c<thisSheet[0].length; c++) {
-				output = output + "|" + thisSheet[c][r].abbreviatedCellText();
+				output = output + "|" + thisSheet[r][c].abbreviatedCellText();
 			}
 			output += "|";
 			output += "\n";
 		}
 		return output;
 	}	
+	private void assignValue(String cell, String input) {
+		SpreadsheetLocation loc = new SpreadsheetLocation(cell);
+		thisSheet[loc.getRow()][loc.getCol()] = new TextCell(input.substring(1, input.length() - 1));
+	}
 	private void clear() {
 		for (int r = 0; r< thisSheet[0].length; r++) {
 			for (int c = 0; c< thisSheet.length; c++) {
 				thisSheet[c][r] = new EmptyCell();
 			}
 		}
-	}
-}
+	}}
