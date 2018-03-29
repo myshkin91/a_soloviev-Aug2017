c5d778f2979626a0997e10b599a2125e5ee85213
Sasha <Sasha@192.168.1.146>
2018-03-28 19:23:54 -0700

    realCell

--- TextExcel/src/textExcel/RealCell.java
+++ TextExcel/src/textExcel/RealCell.java
@@ -1,5 +1,28 @@
 package textExcel;
 
-public class RealCell {
+public abstract class RealCell implements Cell{
+	private String input;
+	public RealCell(String input) {
+		this.input = input;
+	}
+	@Override
+	public abstract String abbreviatedCellText();
+	
+	@Override
+	public abstract String fullCellText();
+	
+	public abstract double getDoubleValue();
+	
+	public String getRealCell() {
+		return input;
+	}
+	
+	public String addSpaces(String text) {
+		while(text.length() <10) {
+			text +=" ";
+		}
+		return text;
+	}
+	
 
 }
