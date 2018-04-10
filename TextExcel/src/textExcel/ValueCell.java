55006e03236b501716b2d96cbdb50541cfbd3cbf
Sasha <Sasha@192.168.1.146>
2018-04-09 22:34:46 -0700

    valueCell

--- TextExcel/src/textExcel/ValueCell.java
+++ TextExcel/src/textExcel/ValueCell.java
@@ -1,5 +1,26 @@
 package textExcel;
 
-public class ValueCell {
+public class ValueCell extends RealCell{
+	
+	public ValueCell(String input) {
+		super(input);
+	}
+	public String abbreviatedCellText() {
+		String cellText = Double.toString(getDoubleValue());
+		if(cellText.length()<10) {
+			if(!cellText.contains(".")) {
+				cellText += ".0";
+			}
+			return addSpaces(cellText);
+		}else {
+			return cellText.substring(0,10);
+		}
+	}
+	public String fullCellText() {
+		return getRealCell();
+	}
+	public double getDoubleValue() {
+		return Double.parseDouble(getRealCell());
+	}
 
 }
