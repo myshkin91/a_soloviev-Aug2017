31024b10f05f28c1461420f9b413662e165150a8
Sasha <Sasha@192.168.1.146>
2018-04-18 23:19:12 -0700

    no changes

--- TextExcel/src/textExcel/PercentCell.java
+++ TextExcel/src/textExcel/PercentCell.java
@@ -6,19 +6,22 @@ public class PercentCell extends RealCell {
 		super(input);
 	}
 	
-	public String abbreviatedCellText() {int decimalPoint = getRealCell().indexOf(".");
-	String wholeNum = getRealCell().substring(0,decimalPoint);
-	if(wholeNum.length() < 10) {
-		wholeNum += "%";
-		return super.addSpaces(wholeNum);
-	}else {
-		return (wholeNum.substring(0,8) + "%");
+	public String abbreviatedCellText() {
+		int decimalPoint = getRealCell().indexOf(".");
+		String wholeNum = getRealCell().substring(0,decimalPoint);
+		if(wholeNum.length() < 10) {
+			wholeNum += "%";
+			return super.addSpaces(wholeNum);
+		}else {
+			return (wholeNum.substring(0,8) + "%");
 		}
 	}
+	
 	public String fullCellText() {
 		String text = getDoubleValue() + "";
 		return text;
 	}
+	
 	public double getDoubleValue() {
 		String num = getRealCell().substring(0,getRealCell().length()-1);
 		return Double.parseDouble(num) / 100.0;
