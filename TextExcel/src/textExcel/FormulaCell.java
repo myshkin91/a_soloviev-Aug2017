59a6e61094ed676915b2c3de6a7d310b3f27288d
Sasha <Sasha@192.168.1.146>
2018-04-23 21:01:55 -0700

    checkpoint 4

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -1,18 +1,56 @@
 package textExcel;
 
+import java.util.ArrayList;
+
 public class FormulaCell extends RealCell {
+	private String input;
 	
+	//constructor
 	public FormulaCell(String input) {
 		super(input);
+		this.input = input;
 	}
 	
 	public String abbreviatedCellText() {
-		return getDoubleValue()+"";
+		String temp = getDoubleValue() + "";
+		for(int i = temp.length();i<10;i++) {
+			temp += " ";
+		}
+		return temp.substring(0,10);
+	}
+	
+	public String fullCellText() {
+		return input;
 	}
 	
 	public double getDoubleValue() {
-		return 0;
+		String formula = input.substring(2, input.length()-2);
+		String [] arrFormula = formula.split(" ");
+		ArrayList<Double> numbers = new ArrayList<Double>();
+		ArrayList<String> operators = new ArrayList<String>();
+		for (int i = 0; i < arrFormula.length; i += 2) {
+			numbers.add(Double.parseDouble(arrFormula[i]));
+			if (i < arrFormula.length - 1) {
+				operators.add(arrFormula[i + 1]);
+				}
+		}	
+		
+		double temp = numbers.get(0);
+		
+		for (int i = 0; i < operators.size(); i++) {
+			if (operators.get(i).equals("+")) {
+				temp += numbers.get(i + 1);
+			} else if (operators.get(i).equals("-")) {
+				temp -= numbers.get(i + 1);
+			} else if (operators.get(i).equals("*")) {
+				temp *= numbers.get(i + 1);
+			}else if (operators.get(i).equals("/")) {
+				temp /= numbers.get(i + 1);
+			}
+		}
+		return temp;
 	}
+	
 	
 
 }
