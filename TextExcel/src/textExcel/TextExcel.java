c8df186645e09070bab987466cd173a6475d1b3a
Sasha <Sasha@192.168.1.146>
2018-04-23 23:29:57 -0700

    minor changes

--- TextExcel/src/textExcel/TextExcel.java
+++ TextExcel/src/textExcel/TextExcel.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class TextExcel{
 
-	public static Spreadsheet sheet1 = new Spreadsheet();
+	/*public static Spreadsheet sheet1 = new Spreadsheet();
 	public static void main(String[] args)
 	{
 		boolean done = false;
@@ -21,4 +21,20 @@ public class TextExcel{
 			}
 		}
 	}
-}
+} */
+	public static void main(String[] args) {
+  		Scanner scanner = new Scanner(System.in);
+  		System.out.println("Welcome to TextExcel! To quit, type \"quit\".");
+  		Spreadsheet spreadsheet = new Spreadsheet();
+  		boolean done = true;
+  		while (done) {
+			System.out.println("Enter a command");
+  			String input = scanner.nextLine();
+  			if (input.equals("quit")) {
+  				done = false;
+ 			} else {
+ 				System.out.println(spreadsheet.processCommand(input));
+ 			}
+ 		}
+ 	}
+ }
